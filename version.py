ca13c137081f2187bafcae7592f5ea1ad13f53eb
Fenhl <fenhl@fenhl.net>
2022-03-12 01:16:50 +0000

    Update for new rando version

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
-__version__ = "2.2.8 Fenhl-3"
+__version__ = "2.2.8 Fenhl-4"
 
-randomizer_version = '6.2.30 Fenhl-9'
-randomizer_commit = '45fa7c8add7de87f65986f8a780e1ce5298b0de4'
+randomizer_version = '6.2.43 Fenhl-1'
+randomizer_commit = '0f6a7555779ddc4cb429c0d940e1226d93c7cd8a'
