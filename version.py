8f8768219603ff9a571986f00c7220bc28bfc802
Fenhl <fenhl@fenhl.net>
2022-03-17 03:07:47 +0000

    Update randomizer

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 __version__ = "2.2.9 Fenhl-3"
 
-randomizer_version = '6.2.43 Fenhl-17'
-randomizer_commit = '4813437761188a5728e7715e11dbc84acc88f9f9'
+randomizer_version = '6.2.43 Fenhl-19'
+randomizer_commit = '48abeccd41dbfb7a5e421923b98415993d4cea0f'
