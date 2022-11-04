85730f5b57228aab2290d4d05961333a8ec22e21
Fenhl <fenhl@fenhl.net>
2022-11-04 20:47:35 +0000

    Update randomizer

--- rslversion.py
+++ rslversion.py
@@ -1,4 +1,4 @@
-__version__ = "2.3.6 Fenhl-2"
+__version__ = "2.3.6 Fenhl-3"
 
-randomizer_version = '6.2.234 Fenhl-1'
-randomizer_commit = 'c8cd6d471b34728efb43292400d2d875f068a3c5'
+randomizer_version = '6.2.234 Fenhl-4'
+randomizer_commit = '4a9cd63d39535af441110b1dcaf985f31e0e3f0b'
