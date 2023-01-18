a47aaf8385bf4552989fca96dfaea3823592f1bf
Fenhl <fenhl@fenhl.net>
2023-01-18 14:17:55 +0000

    Reenable Nayru's Love in OHKO in Pictionary

--- sort-override.py
+++ sort-override.py
@@ -13,13 +13,14 @@ def sorted_weights(weights):
     return {**weights, 'weights': {setting_name: weights['weights'][setting_name] for setting_name in sorted_keys}}
 
 if __name__ == '__main__':
-    with open('weights/fenhl_override.json', encoding='utf-8') as f:
-        weights_text = f.read()
-    weights = json.loads(weights_text)
-    if '--hook' in sys.argv[1:]:
-        if json.dumps(sorted_weights(weights), indent=4) + '\n' != weights_text:
-            raise ValueError('weights not sorted correctly, run .\\sort-override.py to fix')
-    else:
-        with open('weights/fenhl_override.json', 'w', encoding='utf-8') as f:
-            json.dump(sorted_weights(weights), f, indent=4)
-            print(file=f) # trailing newline
+    for override_name in ('fenhl', 'pictionary'):
+        with open(f'weights/{override_name}_override.json', encoding='utf-8') as f:
+            weights_text = f.read()
+        weights = json.loads(weights_text)
+        if '--hook' in sys.argv[1:]:
+            if json.dumps(sorted_weights(weights), indent=4) + '\n' != weights_text:
+                raise ValueError('weights not sorted correctly, run .\\sort-override.py to fix')
+        else:
+            with open(f'weights/{override_name}_override.json', 'w', encoding='utf-8') as f:
+                json.dump(sorted_weights(weights), f, indent=4)
+                print(file=f) # trailing newline
