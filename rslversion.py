360555ecdc61cbf720f0672522dc748bb2efc60a
Fenhl <fenhl@fenhl.net>
2023-01-26 05:29:27 +0000

    Update randomizer

--- rslversion.py
+++ rslversion.py
@@ -1,4 +1,4 @@
-__version__ = "2.3.8 Fenhl-7"
+__version__ = "2.3.8 Fenhl-8"
 
-randomizer_version = '7.1.32 Fenhl-4'
-randomizer_commit = 'ed1ad237f6929c2d253de1f214d2ae7abc5c8fe7'
+randomizer_version = '7.1.32 Fenhl-5'
+randomizer_commit = '9ba0f177c422f8ecc861341c9fad8ab6fcc48203'
