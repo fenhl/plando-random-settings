2e99dd518d90a31161dd8a339470aa0c741e80cf
Fenhl <fenhl@fenhl.net>
2025-01-09 07:11:43 +0000

    Update randomizer

--- rslversion.py
+++ rslversion.py
@@ -1,9 +1,9 @@
-__version__ = "2.7.4 devmvp-1"
+__version__ = "2.7.4 devmvp-2"
 
 # Randomizer Info
 randomizer_repo = 'OoTRandomizer/OoT-Randomizer'
-randomizer_version = '8.2.37 f.LUM'
-randomizer_commit = 'b499369802ce7ff19f93e4d45c0099f3e6f36acb'
+randomizer_version = '8.2.46 f.LUM'
+randomizer_commit = '5a604ebda43fc0d56e813503666bc0d61bea5185'
 
 # Requirements
 MIN_PY_VERSION = (3, 9)
