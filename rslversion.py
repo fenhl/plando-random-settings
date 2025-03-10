d6484093374c6e51dade08e2426fb379ec902a02
Fenhl <fenhl@fenhl.net>
2025-03-10 01:54:26 +0000

    Update randomizer

--- rslversion.py
+++ rslversion.py
@@ -1,9 +1,9 @@
-__version__ = "2.8.2 Fenhl-4"
+__version__ = "2.8.2 Fenhl-5"
 
 # Randomizer Info
 randomizer_repo = 'fenhl/OoT-Randomizer'
-randomizer_version = '8.2.55 Fenhl-1'
-randomizer_commit = '4fc8707c1783ce43e728247ccd125a7cf55b8b78'
+randomizer_version = '8.2.64 Fenhl-1'
+randomizer_commit = 'dbeb4d2bd4bb065837ce0a9c1ce170784bf4feaa'
 
 # Requirements
 MIN_PY_VERSION = (3, 9)
